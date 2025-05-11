f442ae974e61418331ee926ebbd6f8ca894b88c9
Dharmik Gadhiya <dharmikgadhiya52525@gmail.com>
2025-05-11 18:21:59 +0530

    chnage link to https

--- src/http/axiosMain.js
+++ src/http/axiosMain.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInspector = axios.create({
-  baseURL: "http://13.203.229.149:4444",
+  baseURL: "https://13.203.229.149:4444",
   headers: {
     "Content-Type": "application/json",
   },
