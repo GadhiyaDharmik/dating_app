6e56839bc74db6dbe8ac8283397941f668212983
GadhiyaDharmik <dharmikgadhiya52525@gmail.com>
2025-07-08 19:40:37 +0530

    change in home page

--- src/pages/dashboard/HomePage.jsx
+++ src/pages/dashboard/HomePage.jsx
@@ -5,6 +5,8 @@ import SearchFilterBar from "../../component/SearchFilterBar";
 import StarRatingBar from "../../component/StarRatingBar";
 import axiosInspector from "../../http/axiosMain.js";
 import { useNavigate } from "react-router-dom";
+import { useAboutContext } from "../../utils/AboutContext.jsx";
+import About from "../../component/About.jsx";
 
 function HomePage() {
   const [profiles, setProfiles] = useState([]);
@@ -14,6 +16,8 @@ function HomePage() {
   const [matchModal, setMatchModal] = useState(null);
   const navigate = useNavigate();
   const observer = useRef();
+  const { ShowAbout, setShowAbout, aboutData, setAboutData } =
+    useAboutContext();
 
   const handleInteract = (targetUserId, action, data) => {
     axiosInspector
@@ -118,84 +122,110 @@ function HomePage() {
       });
   };
 
+  const handleShowAboutUser = (userId) => {
+    axiosInspector
+      .get(`/users/${userId}/info`)
+      .then((res) => {
+        console.log("resresresres", res.data);
+        // setShowAbout(true)
+        setShowAbout(true);
+        setAboutData(res.data);
+        // setMatchModal(null);
+        // navigate("/dashboard/messages");
+      })
+      .catch((err) => {
+        console.error("Failed to create or get chat room", err);
+        alert("Something went wrong. Try again!");
+      });
+  };
+
   return (
     <>
-      <div className="flex bg-gray-100 min-h-screen">
-        <div className="flex flex-col w-full gap-4 max-h-screen">
-          {/* <StarRatingBar /> */}
-          <SearchFilterBar
-            profiles={profiles}
-            setProfiles={setProfiles}
-            setLoading={setLoading}
-          />
+      {ShowAbout ? (
+        <About aboutData={aboutData} />
+      ) : (
+        <>
+          <div className="flex bg-gray-100 min-h-screen">
+            <div className="flex flex-col w-full gap-4 max-h-screen">
+              {/* <StarRatingBar /> */}
+              <SearchFilterBar
+                profiles={profiles}
+                setProfiles={setProfiles}
+                setLoading={setLoading}
+              />
 
-          <div className="flex-1 overflow-y-auto px-10 pt-2 pb-4 custom-scroll">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-              {loading && profiles.length === 0 ? (
-                <p className="col-span-full text-center text-gray-500">
-                  Loading...
-                </p>
-              ) : profiles.length === 0 ? (
-                <p className="col-span-full text-center text-gray-500">
-                  No matches found.
-                </p>
-              ) : (
-                profiles.map((profile, index) => {
-                  const isLast = profiles.length === index + 1;
-                  return (
-                    <div
-                      key={profile.id}
-                      ref={isLast ? lastProfileRef : null}
-                    >
-                      <ProfileCard
-                        id={profile.id}
-                        name={profile.name}
-                        age={profile.age}
-                        image={profile.profile_picture}
-                        distance={"N/A"}
-                        interests={
-                          profile.interests
-                            ?.map((i) => i.e_name)
-                            .join(", ") || "N/A"
-                        }
-                        occupation={
-                          Array.isArray(profile.occupation)
-                            ? profile.occupation
-                                .slice(0, 2)
-                                .map((item) => item.e_name)
-                                .join(", ") +
-                              (profile.occupation.length > 2 ? "..." : "")
-                            : profile.occupation?.e_name || "N/A"
-                        }
-                        onInteract={handleInteract}
-                      />
-                    </div>
-                  );
-                })
-              )}
+              <div className="flex-1 overflow-y-auto px-10 pt-2 pb-4 custom-scroll">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                  {loading && profiles.length === 0 ? (
+                    <p className="col-span-full text-center text-gray-500">
+                      Loading...
+                    </p>
+                  ) : profiles.length === 0 ? (
+                    <p className="col-span-full text-center text-gray-500">
+                      No matches found.
+                    </p>
+                  ) : (
+                    profiles.map((profile, index) => {
+                      const isLast = profiles.length === index + 1;
+                      return (
+                        <div
+                          key={profile.id}
+                          ref={isLast ? lastProfileRef : null}
+                        >
+                          <ProfileCard
+                            id={profile.id}
+                            name={profile.name}
+                            age={profile.age}
+                            image={profile.profile_picture}
+                            distance={"N/A"}
+                            interests={
+                              profile.interests
+                                ?.map((i) => i.e_name)
+                                .join(", ") || "N/A"
+                            }
+                            occupation={
+                              Array.isArray(profile.occupation)
+                                ? profile.occupation
+                                    .slice(0, 2)
+                                    .map((item) => item.e_name)
+                                    .join(", ") +
+                                  (profile.occupation.length > 2 ? "..." : "")
+                                : profile.occupation?.e_name || "N/A"
+                            }
+                            onInteract={handleInteract}
+                            handleShowAboutUser={(id) =>
+                              handleShowAboutUser(id)
+                            }
+                          />
+                        </div>
+                      );
+                    })
+                  )}
+                </div>
+                {loading && profiles.length > 0 && (
+                  <p className="text-center text-sm text-gray-400 mt-4">
+                    Loading more...
+                  </p>
+                )}
+              </div>
             </div>
-            {loading && profiles.length > 0 && (
-              <p className="text-center text-sm text-gray-400 mt-4">
-                Loading more...
-              </p>
-            )}
           </div>
-        </div>
-      </div>
 
-      {matchModal && (
-        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 bg-white shadow-xl rounded-2xl p-6 w-[90%] max-w-sm text-center border border-blue-200 z-[9999]">
-          <p className="text-lg text-blue-600 font-semibold mb-3">
-            You and <span className="capitalize">{matchModal.name}</span> liked
-            each other
-          </p>
-          <button
-            className="bg-gradient-to-r from-cyan-500 to-blue-500 text-white font-bold py-2 px-6 rounded-full"
-            onClick={handleSayHello}
-          >
-            SAY HELLO!
-          </button>
-        </div>
+          {matchModal && (
+            <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 bg-white shadow-xl rounded-2xl p-6 w-[90%] max-w-sm text-center border border-blue-200 z-[9999]">
+              <p className="text-lg text-blue-600 font-semibold mb-3">
+                You and <span className="capitalize">{matchModal.name}</span>{" "}
+                liked each other
+              </p>
+              <button
+                className="bg-gradient-to-r from-cyan-500 to-blue-500 text-white font-bold py-2 px-6 rounded-full"
+                onClick={handleSayHello}
+              >
+                SAY HELLO!
+              </button>
+            </div>
+          )}
+        </>
       )}
     </>
   );
